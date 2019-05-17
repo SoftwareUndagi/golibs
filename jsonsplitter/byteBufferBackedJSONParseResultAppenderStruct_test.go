8d4a1d537325f2b630d194b38b4b0cf4788cf6ce
gedesutarsa <gede.sutarsa@gmail.com>
2019-05-17 14:34:00 +0800

    Migrated json spliiter

---
+++ jsonsplitter/byteBufferBackedJSONParseResultAppenderStruct_test.go
@@ -0,0 +1,16 @@
+package jsonsplitter
+
+import (
+	"os"
+	"testing"
+)
+
+func TestByteBufferAppenderToFile(t *testing.T) {
+	appender := NewByteBufferJSONParseResultAppender()
+	appender.Append([]byte("dodol"))
+	appender.Append([]byte("garut"))
+	appender.WriteToFile(appendFilePath(os.TempDir(), "byte-to-file.txt"))
+
+}
+
+
---
+++ jsonsplitter/customFunctionJsonParseResultAppender.go
@@ -0,0 +1,7 @@
+package jsonsplitter
+
+//customFunctionJSONParseResultAppender appender custom. append di handle dengan method custom
+type customFunctionJSONParseResultAppender struct {
+	//actualAppender actual appender yang bertugas append json data
+	actualAppender *AppenderFunction
+}
